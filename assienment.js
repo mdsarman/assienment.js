a4de57a65940e75c7f583f9f543d5d9a64b2fb3b
mdsarman <sarmankhan654@gmail.com>
2021-01-22 21:13:40 +0600

    new

---
+++ assienment.js
@@ -0,0 +1,52 @@
+
+
+
+// KilometertoMeter//
+function kilometerToMeter(kilometer) {
+
+    var Meter = kilometer * 1000;
+    return Meter;
+    
+
+}
+
+var Dhakatocomila = kilometerToMeter(20);
+console.log(Dhakatocomila);
+
+
+
+
+//budgetcalculator//
+function budgetCalculator(watch, mobile, laptop){
+
+    var price = watch + mobile + laptop;
+    return price;
+}
+var sum =budgetCalculator(50, 150, 500);
+console.log(sum);
+
+
+
+
+
+
+//hotelCost//
+function hotelCost(firsttendays, secondtendays, thirdtendays) {
+
+    var rate = firsttendays + secondtendays + thirdtendays;
+    return rate;
+    
+}
+var sum =hotelCost(200*10, 180*10, 150*10);
+console.log(sum);
+
+
+
+
+//Megafriend//
+
+function megaFriend() {
+    var friendsname =[mojid, kalam, jalaam, king, abbir, shonali, motaleb, papiya, sarman, mojiburrahaman , monir,]
+    return friendsname;
+}
+var 
